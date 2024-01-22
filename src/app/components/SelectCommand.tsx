180ecfe6a9993bd9591a4d11cd213438b3a3abbd
Devesh <deves125@gmail.com>
2024-01-22 21:13:03 +0530

    fix: change command to select

--- src/app/components/SelectCommand.tsx
+++ src/app/components/SelectCommand.tsx
@@ -2,96 +2,38 @@
 
 import * as React from "react";
 
-import { Button } from "@/app/components/ui/button";
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/app/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/app/components/ui/popover";
-
-type Status = {
-  value: string;
-  label: string;
-};
-
-const statuses: Status[] = [
-  {
-    value: "pick",
-    label: "Pick",
-  },
-  {
-    value: "reword",
-    label: "Reword",
-  },
-  {
-    value: "edit",
-    label: "Edit",
-  },
-  {
-    value: "squash",
-    label: "Squash",
-  },
-];
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/components/ui/select";
 
 const SelectCommand = () => {
-  const [open, setOpen] = React.useState(false);
-  const [selectedStatus, setSelectedStatus] = React.useState<Status | null>({
-    value: "pick",
-    label: "Pick",
-  });
+  const [command, setCommand] = React.useState("pick");
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button variant="outline" className="w-[100px] justify-start">
-          {selectedStatus?.label}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0" align="start">
-        <StatusList setOpen={setOpen} setSelectedStatus={setSelectedStatus} />
-      </PopoverContent>
-    </Popover>
+    <Select
+      onValueChange={(value) => {
+        setCommand(value);
+      }}
+      defaultValue={"pick"}>
+      <SelectTrigger className="w-[110px]">
+        <SelectValue placeholder="Command" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="pick">Pick</SelectItem>
+          <SelectItem value="reword">Reword</SelectItem>
+          <SelectItem value="edit">Edit</SelectItem>
+          <SelectItem value="squash">Squash</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
   );
 };
-
-function StatusList({
-  setOpen,
-  setSelectedStatus,
-}: {
-  setOpen: (open: boolean) => void;
-  setSelectedStatus: (status: Status | null) => void;
-}) {
-  return (
-    <Command>
-      <CommandInput placeholder="Filter commands..." />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup>
-          {statuses.map((status) => (
-            <CommandItem
-              key={status.value}
-              value={status.value}
-              onSelect={(value) => {
-                setSelectedStatus(
-                  statuses.find((priority) => priority.value === value) || null
-                );
-                setOpen(false);
-              }}>
-              {status.label}
-            </CommandItem>
-          ))}
-        </CommandGroup>
-      </CommandList>
-    </Command>
-  );
-}
 
 export default SelectCommand;
