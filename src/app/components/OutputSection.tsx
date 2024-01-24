de8dd2e4d3a1fb82405e41fcd91e6c48d22bfe73
Devesh <deves125@gmail.com>
2024-01-25 00:08:11 +0530

    feat: add copy to clipboard button

--- src/app/components/OutputSection.tsx
+++ src/app/components/OutputSection.tsx
@@ -7,11 +7,15 @@ import convertJsonToGit from "../utils/parseJsonToGit";
 
 import "ace-builds/src-noconflict/mode-markdown";
 import "ace-builds/src-noconflict/theme-twilight";
+import { Button } from "./ui/button";
+import { ToastAction } from "@/app/components/ui/toast";
+import { useToast } from "@/app/components/ui/use-toast";
 
 const OutputSection = () => {
   const { state } = useAppContext();
   const [outputData, setOutputData] = useState<string>("");
   const aceEditorRef = useRef<any>(null);
+  const { toast } = useToast();
 
   useEffect(() => {
     const gitString = convertJsonToGit(state.currentData);
@@ -21,21 +25,37 @@ const OutputSection = () => {
     }
   }, [state.outputData]);
 
+  function copyOutputToClipboard() {
+    if (outputData) {
+      navigator.clipboard.writeText(outputData);
+
+      toast({
+        description: "Commands copied to clipboard",
+      });
+    }
+  }
+
   return (
-    <div>
-      <div>
-        <AceEditor
-          ref={aceEditorRef}
-          theme="twilight"
-          mode="markdown"
-          width="40vw"
-          height="75vh"
-          readOnly
-          defaultValue={outputData}
-          name="REBASE_OUTPUT"
-          fontSize={18}
-          editorProps={{ $blockScrolling: false }}
-        />
+    <div className="w-full h-full flex flex-col">
+      <div className="bg-background w-full p-4 h-fit flex items-center justify-center outline outline-1">
+        <Button onClick={copyOutputToClipboard}>Copy To Clipboard</Button>
+      </div>
+
+      <div className="bg-background flex items-center justify-center h-full px-4 outline outline-1">
+        <div className="p-4">
+          <AceEditor
+            ref={aceEditorRef}
+            theme="twilight"
+            mode="markdown"
+            width="42vw"
+            height="72vh"
+            readOnly
+            defaultValue={outputData}
+            name="REBASE_OUTPUT"
+            fontSize={18}
+            editorProps={{ $blockScrolling: false }}
+          />
+        </div>
       </div>
       {/* <GitHubCalendar username="grubersjoe" /> */}
     </div>
