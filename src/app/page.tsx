10c481606581be79219680122036244fa0f05cec
Devesh <deves125@gmail.com>
2024-01-24 14:24:03 +0530

    fix: layout style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,28 @@ import Run from "./components/Run";
 
 export default function Home() {
   return (
-    <div className="flex flex-col bg-purple-500 w-screen h-screen">
-      <div className="flex flex-row bg-red-500 w-screen h-24 items-center justify-center">
+    //
+    <div className="bg-slate-400 flex flex-col h-screen w-full">
+      <div className="bg-red-300 flex flex-row w-screen h-fit items-center justify-center">
         {/* heading */}
-        Rewrite Git History
+        <div className="py-4">Rewrite Git History</div>
       </div>
-      <div className="bg-gray-400  flex flex-row w-screen h-full">
-        <div className="bg-slate-400 w-1/2 h-full items-center justify-center flex">
+      <div className="bg-red-400  flex flex-row flex-grow w-screen h-fit">
+        <div className=" w-full h-full items-center justify-center flex">
           <InputSection />
         </div>
 
-        <div className="bg-teal-500 w-24 flex items-center justify-center">
+        <div className="bg-lime-300 w-[10%] flex items-center justify-center">
           <Run />
         </div>
-        <div className="bg-gray-600 w-1/2 h-full flex items-center justify-center">
+        <div className="w-full h-min flex items-center justify-center">
           <OutputSection />
         </div>
       </div>
 
-      <div className="h-12 flex items-center justify-center">Made with ❤️</div>
+      <div className=" bg-teal-300 h-fit flex items-center justify-center ">
+        <div className="py-2">Made with ❤️</div>
+      </div>
     </div>
   );
 }
